b8702100348e57e37edca1d8fc1df24ed633ab1e
villegas07 <brayanvillegas0719@gmail.com>
2025-11-28 04:38:39 -0500

    correciones

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,12 +7,14 @@ import { NotesModule } from './modules/notes/notes.module';
 import { CategoriesModule } from './modules/categories/categories.module';
 import { ResponseInterceptor } from './core/interceptors/response.interceptor';
 import { APP_INTERCEPTOR } from '@nestjs/core';
+import { PrismaService } from './database/prisma.service';
 
 @Module({
   imports: [AuthModule, UsersModule, NotesModule, CategoriesModule],
   controllers: [AppController],
   providers: [
     AppService,
+    PrismaService,
     {
       provide: APP_INTERCEPTOR,
       useClass: ResponseInterceptor,
