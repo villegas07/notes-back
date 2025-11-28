0441ab73bd7b5b04d10ae3611a04996be41c0b88
villegas07 <brayanvillegas0719@gmail.com>
2025-11-28 04:19:11 -0500

    envio de correos

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3002',
+    origin: ['http://localhost:3002', 'https://notes-back-a53g.onrender.com'],
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
