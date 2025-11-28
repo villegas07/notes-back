d12a620940410b461bbeb233d3279fe133b17b51
villegas07 <brayanvillegas0719@gmail.com>
2025-11-28 17:40:33 -0500

    correccion en las migraciones por color

--- prisma/migrations/20251128222618_add_category_color/migration.sql
+++ prisma/migrations/20251128222618_add_category_color/migration.sql
@@ -4,5 +4,3 @@
   - Added the required column `color` to the `categories` table without a default value. This is not possible if the table is not empty.
 
 */
--- AlterTable
-ALTER TABLE "categories" ADD COLUMN     "color" TEXT NOT NULL;
