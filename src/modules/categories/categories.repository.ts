17d188367fb79aa7c4fa91f1d1a9243cc155dc05
villegas07 <brayanvillegas0719@gmail.com>
2025-11-28 18:48:09 -0500

    correccion en migrations category

--- src/modules/categories/categories.repository.ts
+++ src/modules/categories/categories.repository.ts
@@ -4,7 +4,7 @@ import { PrismaService } from '../../database/prisma.service';
 export interface Category {
   id: string;
   name: string;
-  color: string;
+  color?: string;
   createdAt: Date;
   updatedAt: Date;
 }
