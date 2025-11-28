986b86d7e98ae544a91332f080b02c7e6541c44a
villegas07 <brayanvillegas0719@gmail.com>
2025-11-28 03:42:43 -0500

    permitir peticiones por el puerto 3002 para desarrollo

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    origin: 'http://localhost:3002',
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
