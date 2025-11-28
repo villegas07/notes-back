84e5900a8b826e9433f84a68fefde23bb693189b
villegas07 <brayanvillegas0719@gmail.com>
2025-11-28 03:24:45 -0500

    ajustes en los permisos CORD

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
