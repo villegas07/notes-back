7e63a4f62ee8182783bad7087c261eb085f9ba41
villegas07 <brayanvillegas0719@gmail.com>
2025-11-29 05:01:47 -0500

    acceso desde vercel

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3002', 'https://notes-back-a53g.onrender.com'],
+    origin: [
+      'http://localhost:3002',
+      'https://notes-back-a53g.onrender.com',
+      'https://notes-gold-five-22.vercel.app'
+    ],
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
